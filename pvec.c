db7533e786d2d94b04ece61223d4e3ffea6a04ce
Patrick <41267865+Patrick-McGuire@users.noreply.github.com>
2021-10-16 10:42:19 -0400

    test

---
+++ pvec.c
@@ -0,0 +1,124 @@
+//
+// Created by patri on 10/15/2021.
+//
+
+#include "pvec.h"
+/* Format for pvec:
+ *  First memory block:
+ *      <64bit address of next element><64bit length>
+ *  Element memory block:
+ *      <64bit address of next element><64bit value>
+ */
+
+uint64_t pvec_countDigits(uint64_t num) {
+    return num < 10 ? num > 0 : pvec_countDigits(num / 10) + 1;
+}
+
+void pvec_print(uint64_t *pvec) {
+    uint32_t digits = 0;                                        // Keep track of the total number of digits we will need to print
+    uint64_t len = pvec_getLength(pvec);                        // Get the number of elements in the pvec
+    for(int i = 0; i < len; i++) {                              // Count the total number of digits
+        uint32_t temp = pvec_countDigits(pvec_getValue(pvec, i));
+        digits += temp == 0 ? 1 : temp;
+    }
+    uint32_t strSize = digits + (len * 2) + 4;                  // Calculate the buffer size needed
+    char buf[strSize];
+    buf[0] = '{';
+    buf[1] = ' ';
+    char *ptr = &buf[2];                                        // Get all values from the pvec and add them to the buffer
+    for(int i = 0; i < len-1; i++) {
+        uint64_t val = pvec_getValue(pvec, i);
+        sprintf(ptr, "%lu, ", val);
+        uint32_t temp = pvec_countDigits(val);
+        ptr = &ptr[(temp == 0 ? 1 : temp) + 2];
+    }
+    sprintf(ptr, "%lu }\n", pvec_getValue(pvec, len-1));
+    printf("%s", buf);                                          // Print the result
+}
+
+uint64_t pvec_getLength(uint64_t *pvec) {
+    return pvec[1];                                 // Retrieve the length of the pvec
+}
+
+uint64_t * pvec_getPointerToElement(uint64_t *pvec, uint64_t index) {
+    uint64_t len = pvec_getLength(pvec);            // Get length of the pvec
+    if(index > len) {                               // Clip the index
+        index = len;
+    }
+    uint64_t *elementPtr = pvec;                    // Current block pointer
+    for(int i = 0; i < index; i++) {
+        elementPtr = (uint64_t*) elementPtr[0];     // Iterate to the next block
+    }
+    return elementPtr;                              // Return pointer to the desired block
+}
+
+uint64_t * pvec_newPvec() {
+    uint64_t *pvecPtr = calloc(2, sizeof(uint64_t));        // Get the memory needed for a array of length 0
+    pvecPtr[1] = 0;                                         // Set the length to 0
+    return pvecPtr;
+}
+
+void pvec_append(uint64_t *pvec, uint64_t val) {
+    uint64_t *lastElePtr = pvec_getPointerToElement(pvec, pvec_getLength(pvec));     // Get the pointer to the last element of the array
+    uint64_t *nextEle = calloc(2, sizeof(uint64_t));                                 // Get the memory for the next element
+    nextEle[1] = val;                                                                // Set the value in the block allocated
+    lastElePtr[0] = (uint64_t) nextEle;                                              // Save the pointer to this block in the last block
+    pvec[1]++;                                                                       // Increment the stored length of the pvec
+}
+
+uint64_t pvec_pop(uint64_t *pvec) {
+    uint64_t *lastElePtr = pvec_getPointerToElement(pvec, pvec_getLength(pvec));     // Get the pointer to the last element of the array
+    uint64_t out = lastElePtr[1];                                                    // Save the last value in the pvec so we can free the memory
+    pvec[1]--;                                                                       // Decrease the length by 1
+    free(lastElePtr);                                                                // Free the memory
+    return out;                                                                      // Return desired value
+}
+
+uint64_t pvec_remove(uint64_t *pvec, uint64_t index) {
+    if(index >= pvec_getLength(pvec) - 1) {                                       // If we are removing the last element we can just pop it
+        return pvec_pop(pvec);
+    }
+    // Get the pointers needed
+    uint64_t *eleToRemove = pvec_getPointerToElement(pvec, index+1);
+    uint64_t *prevEle = pvec_getPointerToElement(pvec, index);
+    uint64_t *nextEle = pvec_getPointerToElement(pvec, index+2);
+    uint64_t out = eleToRemove[1];                                                // Save the value to return
+    prevEle[0] = (uint64_t) nextEle;                                              // Adjust the pointer saved in the previous element to reference the next element
+    pvec[1]--;                                                                    // Adjust the length of the array
+    free(eleToRemove);                                                            // Free the now unused memory
+    return out;
+}
+
+void pvec_insert(uint64_t *pvec, uint64_t index, uint64_t val) {
+    if(index >= pvec_getLength(pvec) - 1) {                                       // If we are insert the last element we can just append it
+        pvec_append(pvec, val);
+        return;
+    }
+    // Get the pointers needed
+    uint64_t *prevEle = pvec_getPointerToElement(pvec, index);
+    uint64_t *nextEle = pvec_getPointerToElement(pvec, index+1);
+    uint64_t *eleToAdd = calloc(2, sizeof(uint64_t));
+    eleToAdd[0] = (uint64_t) nextEle;
+    eleToAdd[1] = val;
+    prevEle[0] = (uint64_t) eleToAdd;
+    pvec[1]++;
+}
+
+uint64_t pvec_getValue(uint64_t *pvec, uint64_t index) {
+    return pvec_getPointerToElement(pvec, index+1)[1];          // Get the pointer to the block at the specified index, then get the value from that block
+}
+
+void pvec_setValue(uint64_t *pvec, uint64_t index, uint64_t val) {
+    pvec_getPointerToElement(pvec, index+1)[1] = val;           // Set the value in the appropriate block
+}
+
+void pvec_delete(uint64_t *pvec) {
+    while (pvec_getLength(pvec) > 0) {                                // Pop every element
+        pvec_pop(pvec);
+    }
+    free(pvec);                                                       // Free the initial block
+}
+
+
+
+
---
+++ pvec.h
@@ -0,0 +1,83 @@
+//
+// Created by patri on 10/15/2021.
+//
+
+#ifndef C_VECTOR_TESTS_PVEC_H
+#define C_VECTOR_TESTS_PVEC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Prints out a pvec
+ * @param pvec pvec to print
+ * @return
+ */
+void pvec_print(uint64_t *pvec);
+
+/**
+ * Creates a new pvec
+ * @return pointer to pvec
+ */
+uint64_t * pvec_newPvec();
+
+/**
+ * Appends a new uint64_t to the pvec
+ * @param[in,out] pvec pvec to append value to
+ * @param[in] val value to append
+ */
+void pvec_append(uint64_t *pvec, uint64_t val);
+
+/**
+ * Removes the last element from the pvec
+ * @param pvec pvec to remove last element from
+ * @return the element removed
+ */
+uint64_t pvec_pop(uint64_t *pvec);
+
+/**
+ * Inserts a value into a pvec at a specified index
+ * @param pvec pvec to insert into
+ * @param index index to insert into
+ * @param val value to insert
+ */
+void pvec_insert(uint64_t *pvec, uint64_t index, uint64_t val);
+
+/**
+ * Removes a value from a pvec at a specified index
+ * @param pvec pvec to remove from
+ * @param index index to remove from
+ * @return value removed
+ */
+uint64_t pvec_remove(uint64_t *pvec, uint64_t index);
+
+/**
+ * Gets a value from the specified pvec
+ * @param[in,out] pvec pvec to get value from
+ * @param[in] index index to retrieve
+ * @return value from pvec
+ */
+uint64_t pvec_getValue(uint64_t *pvec, uint64_t index);
+
+/**
+ * Sets a value in the specified pvec
+ * @param pvec pvec to set value to
+ * @param index index to set
+ * @param val value to set
+ */
+void pvec_setValue(uint64_t *pvec, uint64_t index, uint64_t val);
+
+/**
+ * Gets the length of a specified pvec
+ * @param pvec pvec to get length from
+ * @return length of specified pvec
+ */
+uint64_t pvec_getLength(uint64_t *pvec);
+
+/**
+ * Deletes pvec by free memory used
+ * @param pvec pvec to free
+ */
+void pvec_delete(uint64_t *pvec);
+
+#endif //C_VECTOR_TESTS_PVEC_H
